e19456885448df9606cd44e283848b09d2f29d21
fortune-0 <fortunepeterspc07@gmail.com>
2023-10-19 13:47:24 -0700

    about to merge

--- shell.h
+++ shell.h
@@ -8,9 +8,20 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include "error.h"
+/* #include "error.h" */
+/* exit.c */
+void bin_exit(general_t *info, char **arguments);
+int number_controller(general_t *info, char *number);
+/* execute.c */
 void executes_commands(const char *input);
+
+/* start_info.c */
+
 void start_info(void);
+
+/* user-inputs */
 void read_command(char *input, size_t size);
+
+/* print_shell.c */
 void shell_print(const char *message);
 #endif /* SHELL_H */
