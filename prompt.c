00969608f06cb54ebec77f2ad8017c54ea50647f
fortune-0 <fortunepeterspc07@gmail.com>
2023-10-20 07:33:48 -0700

    new

---
+++ prompt.c
@@ -0,0 +1,12 @@
+#include "shell.h"
+
+/**
+ *Display_prompt - function to display prompt
+ *@~$: displayed symbol
+ *Return: runs till infinity
+ */
+
+void display_prompt(void)
+{
+	printpr("simple_shell$");
+}
